16ddfeed53950b7da6a19af4f77c9806fccb2d14
erickfranca <erickartdsgn@gmail.com>
2026-07-25 09:51:38 -0300

    updating RestClientConfig.java

--- src/main/java/com/efranca/orderservice/config/RestClientConfig.java
+++ src/main/java/com/efranca/orderservice/config/RestClientConfig.java
@@ -9,24 +9,29 @@ import org.springframework.web.client.RestClient;
  * RestClient e a API sincrona recomendada pelo Spring (substitui o
  * RestTemplate, que esta em modo de manutencao). Cada bean aqui aponta
  * para um servico downstream diferente - e essas chamadas HTTP sincronas
- * sao exatamente o que faz o trace "atravessar" processos: quando voce
- * habilitar tracing, o Spring instrumenta esse client automaticamente
- * para propagar o header de trace (traceparent) na requisicao de saida.
+ * sao exatamente o que faz o trace "atravessar" processos.
+ *
+ * IMPORTANTE: injetamos o RestClient.Builder (em vez de chamar o metodo
+ * estatico RestClient.builder()). O builder injetado e auto-configurado
+ * pelo Spring Boot com toda a infraestrutura de observabilidade - e essa
+ * infraestrutura e o que insere o header "traceparent" automaticamente
+ * na chamada de saida quando o tracing estiver habilitado. Se voce criar
+ * o client a partir do builder estatico, essa propagacao nao acontece.
  */
 @Configuration
 @EnableConfigurationProperties(ServiceUrlsProperties.class)
 public class RestClientConfig {
 
     @Bean
-    public RestClient paymentRestClient(ServiceUrlsProperties properties) {
-        return RestClient.builder()
+    public RestClient paymentRestClient(RestClient.Builder builder, ServiceUrlsProperties properties) {
+        return builder
                 .baseUrl(properties.getPaymentUrl())
                 .build();
     }
 
     @Bean
-    public RestClient shippingRestClient(ServiceUrlsProperties properties) {
-        return RestClient.builder()
+    public RestClient shippingRestClient(RestClient.Builder builder, ServiceUrlsProperties properties) {
+        return builder
                 .baseUrl(properties.getShippingUrl())
                 .build();
     }
